de186d533216c31aaf241f0d73766d62474ac2c4
IND <banerjee0991@outlook.com>
2023-01-15 16:39:03 +0530

    Git change whole TCs to OrangeHRM

--- SeleniumCSAutomation/src/test/java/cs/qa/te/tests/LoginPageTest.java
+++ SeleniumCSAutomation/src/test/java/cs/qa/te/tests/LoginPageTest.java
@@ -20,61 +20,20 @@ public class LoginPageTest extends BaseTest {
 	// Navigate to the Login page
 	@BeforeClass(groups= {"Smoke", "Regression"}, enabled = true)
 	public void clickToLoginLink() throws  InterruptedException{
-		loginPage.closePopUp();
+		
 		loginPage.clickLogin();
 		
 	}
 	
 	
 	//For Language settings
-	@Test(groups= {"Smoke", "Regression"},enabled = true, priority=2)
+	@Test(enabled = true, priority=2)
 	public void changeLanguageTest(){
-			loginPage.changeLanguage();
+			String text = loginPage.getText();
+			
+			System.out.println(text);
 		}
 		
 		
-
-	
-	// Confirm Company logo
-	@Test(groups= {"Smoke", "Regression"}, enabled = true, priority=5)
-	public void loginPageLogoTest() {
-		String urlText = loginPage.logoUrl().trim();
-		System.out.println(urlText);
-		System.out.println(Constants.LOGIN_PAGE_LOGO_URLText);
-		//softAssert.assertTrue(urlText.contains(Constants.LOGIN_PAGE_LOGO_URLText));
-		//softAssert.assertAll();
-		Verify.verify(urlText.equalsIgnoreCase(Constants.LOGIN_PAGE_LOGO_URLText),"not matched");
-			
-		}
-		
-	
-		
-	//Get the login page above link list
-	//Need to check might be not 
-	
-			
-		
-	
-	//Resource drop downn list
-	
-	
-	//Click on Resource drop down list options
-	
-	
-	/*
-	 * 
-	 * 
-	 * This code will be used when "Translation (Priority =10 & Priority =11 )" not to be tested for TE login time
-	 
-	@Test(enabled = false, priority=10)
-	public void loginPageSignInTest() {
-		loginPage.doLogin(prop.getProperty("username").trim(), prop.getProperty("password").trim());
-		
-	}
-	
-	*
-	*
-	*/
-	
 	
 }
